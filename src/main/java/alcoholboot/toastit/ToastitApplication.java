d96e0e3bfd09e697292cc5a94b494ccef38fc68c
CodeJesterND <dev.hyoseung@gmail.com>
2024-08-01 17:21:04 +0900

    🔧 Config : 프로젝트 초기 구성 파일 추가
    프로젝트 초기 환경설정 구성 파일 추가 완료 :)
    
    Resolves : #1

---
+++ src/main/java/alcoholboot/toastit/ToastitApplication.java
@@ -0,0 +1,13 @@
+package alcoholboot.toastit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToastitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alcoholboot/toastit/ToastitApplicationTests.java
@@ -0,0 +1,13 @@
+package alcoholboot.toastit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToastitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
