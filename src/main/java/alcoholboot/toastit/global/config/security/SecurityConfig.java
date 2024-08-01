572a7c59bb1a8ec1af96ecea213c15496ccc2f8e
CodeJesterND <dev.hyoseung@gmail.com>
2024-08-01 11:58:21 +0900

    ⚙️  Config : 프로젝트 초기 환경설정
    redis, security 구성파일 추가 완료
    
    Ref: #1

---
+++ src/main/java/alcoholboot/toastit/global/config/security/SecurityConfig.java
@@ -0,0 +1,80 @@
+package alcoholboot.toastit.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Spring Security 설정 클래스
+ * 애플리케이션의 보안 구성을 정의
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    // JWT util
+//    private final JwtTokenizer jwtTokenizer;
+
+    // 모든 유저 허용 페이지
+    String[] allAllowPage = new String[]{
+            "/", // 메인페이지
+            "/error", // 에러페이지
+            "/test/**", // 테스트 페이지
+            "/user/refreshToken", // 토큰 재발급 페이지
+            "/user/authEmail", // 인증 메일 페이지
+            "/resources/image/**", // 이미지 리소스
+            "/image/**", // 이미지 url 임시 허용
+    };
+
+    // 비로그인 유저 허용 페이지
+    String[] notLoggedAllowPage = new String[]{
+            "/user/login", // 로그인 페이지
+            "/user/join" // 회원가입 페이지
+    };
+
+    /**
+     * 보안 필터 체인
+     *
+     * @param http 수정할 HttpSecurity 객체
+     * @return 구성된 SecurityFilterChain
+     * @throws Exception HttpSecurity 구성 시 발생한 예외
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 유저별 페이지 접근 허용
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(allAllowPage).permitAll() // 모든 유저
+                .requestMatchers(notLoggedAllowPage).not().authenticated() // 비로그인 유저
+                .anyRequest().authenticated()
+        );
+
+        // 세션 관리 Stateless 설정(서버가 클라이언트 상태 저장x)
+        http.sessionManagement(auth -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // cors 허용
+        http.csrf(csrf -> csrf.disable());
+
+        // 로그인 폼 비활성화
+        http.formLogin(auth -> auth.disable());
+
+        // http 기본 인증(헤더) 비활성화
+        http.httpBasic(auth -> auth.disable());
+
+        // JWT 필터 사용
+//        http.addFilterBefore(new TokenAuthenticationFilter(jwtTokenizer), UsernamePasswordAuthenticationFilter.class);
+
+        // SecurityFilterChain을 빌드 후 반환
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
