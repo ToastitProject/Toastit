a1fb6b57f95b075b9e261fbc7fa88187ce56e11c
CodeJesterND <dev.hyoseung@gmail.com>
2024-08-05 09:58:36 +0900

    🎨 Style : "메인 애플리케이션 파일 줄바꿈 수정"
    12줄 코드라인 여백 제거 했습니다 :)

--- src/main/java/alcoholboot/toastit/ToastitApplication.java
+++ src/main/java/alcoholboot/toastit/ToastitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToastitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ToastitApplication.class, args);
     }
-
 }
