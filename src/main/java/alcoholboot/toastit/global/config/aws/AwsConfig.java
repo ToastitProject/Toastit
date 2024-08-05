7ec1991a732b9c2370ef3d78e93e34b8f339df81
sinwoongkang1 <kkace22@gmail.com>
2024-08-05 15:51:00 +0900

    ✨ Feat : 새로운 기능 추가 AWS 설정 클래스 추가 완료(연결 객체 생성) Ref : #3

---
+++ src/main/java/alcoholboot/toastit/global/config/aws/AwsConfig.java
@@ -0,0 +1,28 @@
+package alcoholboot.toastit.global.config.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AwsConfig {
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String iamAccessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String iamSecretKey;
+
+    private String region = "ap-northeast-2";
+
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(iamAccessKey, iamSecretKey);
+        return (AmazonS3Client)AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}
