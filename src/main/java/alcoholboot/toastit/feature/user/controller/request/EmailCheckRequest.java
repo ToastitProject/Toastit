5b1f576acb101d7664b2e37a475d01356f9f1b15
CodeJesterND <dev.hyoseung@gmail.com>
2024-08-13 22:02:59 +0900

    🚑 Fix : "브라우저 JSON 리얼라이즈 오류 해결"
    - DTO 기본 생정자 추가
    
    Ref : #6

--- src/main/java/alcoholboot/toastit/feature/user/controller/request/EmailCheckRequest.java
+++ src/main/java/alcoholboot/toastit/feature/user/controller/request/EmailCheckRequest.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 이메일 인증번호 체크 dto
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailCheckRequest {
     @NotEmpty(message = "이메일을 입력해주세요.")
     @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$",
