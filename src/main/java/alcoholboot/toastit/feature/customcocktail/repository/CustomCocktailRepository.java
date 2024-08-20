47ee91b6fed030dbdef1e2e4b0f15bc94bee1e86
sinwoongkang1 <kkace22@gmail.com>
2024-08-20 16:50:36 +0900

    ✨ Feat : 커스텀 칵테일을 찾는 메서드 추가 user id 와 custom cocktail id 로 칵테일 엔티티를 찾는 메서드 추가

--- src/main/java/alcoholboot/toastit/feature/customcocktail/repository/CustomCocktailRepository.java
+++ src/main/java/alcoholboot/toastit/feature/customcocktail/repository/CustomCocktailRepository.java
@@ -1,10 +1,14 @@
 package alcoholboot.toastit.feature.customcocktail.repository;
 
 import alcoholboot.toastit.feature.customcocktail.domain.CustomCocktail;
+import alcoholboot.toastit.feature.user.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomCocktailRepository extends CrudRepository<CustomCocktail, Long> {
-
+Optional<UserEntity> findUserByName(String username);
+CustomCocktail findIdByName(String cocktailName);
 }
