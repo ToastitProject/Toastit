9b9299778cbb23fdc23a444c1965e244cf035d58
CodeJesterND <dev.hyoseung@gmail.com>
2024-08-24 22:51:53 +0900

    🚑 Hotfix : "@EnableMongoAudititng 어노테이션 중복 수정"
    - 어노테이션 중복으로 인해 프로젝트 다운 버그 발생 이슈 수정
    
    Ref : #5

--- src/main/java/alcoholboot/toastit/ToastitApplication.java
+++ src/main/java/alcoholboot/toastit/ToastitApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@EnableMongoAuditing
 @EnableJpaAuditing
 @SpringBootApplication
 public class ToastitApplication {
