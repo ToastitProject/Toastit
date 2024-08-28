7dd79784b7eb966ef98dea9c82a6f829fa5fa693
zihang <tlswlgkd565@gmail.com>
2024-08-28 15:45:56 +0900

    🐛 Fix: "특정 시간에 날씨 api가 제대로 호출되지 않는 문제 수정"
    Ref : #47

--- src/main/java/alcoholboot/toastit/feature/api/controller/AreaWeatherController.java
+++ src/main/java/alcoholboot/toastit/feature/api/controller/AreaWeatherController.java
@@ -6,6 +6,8 @@ import alcoholboot.toastit.feature.api.dto.WeatherDTO;
 import alcoholboot.toastit.feature.api.service.RecommendByWeatherService;
 import alcoholboot.toastit.feature.api.service.WeatherService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import alcoholboot.toastit.feature.defaultcocktail.service.CocktailService;
+import alcoholboot.toastit.feature.defaultcocktail.domain.Cocktail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
@@ -31,6 +34,7 @@ import java.util.Random;
 public class AreaWeatherController {
     private final WeatherService weatherService;
     private final RecommendByWeatherService recommendByWeatherService;
+    private final CocktailService cocktailService;
 
     @Value("${google.maps.api.key}")
     private String mapsApiKey;
@@ -50,25 +54,19 @@ public class AreaWeatherController {
     }
 
     @PostMapping("/save-coordinates")
-    public String saveCoordinates(@RequestBody Map<String, Double> coordinates) throws UnsupportedEncodingException, URISyntaxException, JsonProcessingException {
+    @ResponseBody
+    public Map<String, Object> saveCoordinates(@RequestBody Map<String, Double> coordinates) throws UnsupportedEncodingException, URISyntaxException, JsonProcessingException {
         Double latitude = coordinates.get("latitude");
         Double longitude = coordinates.get("longitude");
 
-        // 받은 위도와 경도를 한번 표시해보기
-//        System.out.println("latitude: " + latitude + " longitude: " + longitude);
-
         // 위도와 경도를 격자 x,y 좌표로 변환하기
         LatXLngY latXLngY = weatherService.convertGRID_GPS(0, latitude, longitude);
-//       System.out.println("x,y : "+latXLngY.getX()+","+latXLngY.getY());
 
         // 변환한 x,y좌표로 api요청을 보내서 날씨 정보를 받기
         // areacode, 날짜, 시간(1시간 단위) 추가로 필요
         AreaRequestDTO areaRequestDTO = new AreaRequestDTO();
         areaRequestDTO.setNx(latXLngY.getStringX());
         areaRequestDTO.setNy(latXLngY.getStringY());
-
-        // areacode 받아와보기
-        //System.out.println("areacode 테스트 : "+weatherService.getAreaCode(latXLngY.getStringX(), latXLngY.getStringY()));
 
         // areacode 추가
         areaRequestDTO.setAreacode(weatherService.getAreaCode(latXLngY.getStringX(), latXLngY.getStringY()).getFirst());
@@ -88,17 +86,21 @@ public class AreaWeatherController {
             basedate += now.getDayOfMonth();
         }
 
-        //System.out.println("basedate test : " + basedate);
-
         // 날짜 추가
         areaRequestDTO.setBaseDate(basedate);
 
         // 시간 받아와보기
         LocalTime nowTime = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH0000");
-        String basetime = nowTime.format(formatter);
+        String basetime = "";
+        int hour = nowTime.getHour();
+        int minute = nowTime.getMinute();
+        if (minute < 11) {
+            hour--;
+            basetime = hour + "0000";
+        } else {
+            basetime = hour + "0000";
+        }
 
-        //System.out.println("basetime test : " + basetime);
 
         // 시간 추가
         areaRequestDTO.setBaseTime(basetime);
@@ -139,12 +141,36 @@ public class AreaWeatherController {
         }
 
         List<String> ingredients = recommendByWeatherService.getIngredientsByWeather(weather);
-        // 출력해보기
-        System.out.println(ingredients);
+        randomInt = random.nextInt(9);
+        String ingredient = ingredients.get(randomInt);
 
         // 선택한 재료로 랜덤 칵테일을 선택하기
-        
+        List<Cocktail> cocktails = cocktailService.getCocktailsByIngredient(ingredient);
+        randomInt = random.nextInt(cocktails.size());
 
-        return "/feature/api/areaweather";
+        if (pty == 0) {
+            weather = "clear";
+        } else if (pty == 1 || pty == 5) {
+            weather = "rain";
+        } else {
+            weather = "snow";
+        }
+
+        // 모델에 넣기
+//        model.addAttribute("temperature", t1h);
+//        model.addAttribute("weatherInfo", weather);
+//        model.addAttribute("cocktailInfo", cocktails.get(randomInt).getStrDrink());
+
+        // 출력해보기
+        //System.out.println("temperature: " + t1h + ", weatherInfo: " + weather + ", cocktailInfo : " + cocktails.get(randomInt).getStrDrink());
+
+        // response로 해보기
+        Map<String, Object> response = new HashMap<>();
+        response.put("temperature", t1h);
+        response.put("weatherInfo", weather);
+        response.put("cocktailInfo", cocktails.get(randomInt).getStrDrink());
+
+//        return "/feature/api/areaweather";
+        return response;
     }
 }
