7b1541d77ec2ede48bbb22235f9f8cac87a0839d
YoungH66 <rladudgns2222@gmail.com>
2024-09-02 06:46:40 +0900

    ♻️ Refactor : "basecocktail 이전 페이지로 돌아가는 기능 조정"
    - 이전 페이지로 돌아가는 기능을 조정했습니다.
      - 기존엔 어디에서 돌아가도 page=0로 돌아갔지만, 이제는 페이징된 주소를 기억합니다.

--- src/main/resources/static/js/basecocktail/detail.js
+++ src/main/resources/static/js/basecocktail/detail.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const backToListButton = document.getElementById('backToList');
     const backToSearchButton = document.getElementById('backToSearchResult');
     const lastSearchUrl = sessionStorage.getItem('lastSearchUrl');
+    const lastListUrl = sessionStorage.getItem('lastListUrl');
 
     // 페이지 로드시 적용, cocktailDetail에서 동작
     // 검색 기록이 있다면, 검색화면으로 돌아가는 버튼 활성화
-    if (lastSearchUrl.includes('/cocktails/complex')) {
+    if (lastSearchUrl && lastSearchUrl.includes('/cocktails/complex')) {
         backToSearchButton.style.display = 'block';
         backToSearchButton.href = lastSearchUrl;
+    }
+
+    // 목록으로 돌아가기
+    if (lastListUrl && lastListUrl.includes('/cocktails/all')) {
+        backToListButton.href = lastListUrl;
+    } else {
+        backToListButton.href = '/cocktails/all';
     }
 });
