10534d40bdfc44b82eb6c5e5fa246ed9a509d89a
zihang <tlswlgkd565@gmail.com>
2024-09-05 15:54:08 +0900

    💡 Comment : "repository 코드에 주석 추가"
    Ref : #121

--- src/main/java/alcoholboot/toastit/feature/weathercocktail/repository/WeatherCocktailRepository.java
+++ src/main/java/alcoholboot/toastit/feature/weathercocktail/repository/WeatherCocktailRepository.java
@@ -4,7 +4,12 @@ import alcoholboot.toastit.feature.weathercocktail.entity.WeatherCocktailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface WeatherCocktailRepository extends JpaRepository<WeatherCocktailEntity, Long> {
+    /**
+     * 날씨에 맞는 칵테일을 재료들을 분류해놓은 테이블인 weather_cocktails에서
+     * 날씨를 통해서 재료들을 가져옴
+     */
     WeatherCocktailEntity findByWeather(String weather);
 }
